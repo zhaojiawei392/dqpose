2fb575dc33bc938f2d3c842093255612b1b70de0
zhaojiawei392 <121145027+zhaojiawei392@users.noreply.github.com>
2024-10-13 18:29:14 +0900

    Add files via upload
    first upload

---
+++ src/dqpose.hpp
@@ -0,0 +1,37 @@
+/** 
+ *     This file is part of dqpose.
+ *  
+ *     dqpose is free software: you can redistribute it and/or modify 
+ *     it under the terms of the GNU General Public License as published 
+ *     by the Free Software Foundation, either version 3 of the License, 
+ *     or (at your option) any later version.
+ *  
+ *     dqpose is distributed in the hope that it will be useful, 
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ *     See the GNU General Public License for more details.
+ *  
+ *     You should have received a copy of the GNU General Public License
+ *     along with dqpose. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ *     \file src/dqpose.hpp
+ *	   \author Jiawei ZHAO
+ *	   \version 2.0
+ *	   \date 2024-2025
+ *
+ *     \brief A header file defining dual-quaternion-based pose operations
+ * 
+ *     This file provides the necessary classes and functions to represent 
+ *     and manipulate 3D poses using dual quaternions in a lightweight 
+ *     multi-platform C++ library.
+ * 
+ *     \cite https://github.com/zhaojiawei392
+ */
+
+#pragma once
+#include "dqpose/quat.hpp"
+#include "dqpose/dualquat.hpp"
+#include "dqpose/pose.hpp"
+
